0d24d4a1306200aabe0afd67bebd85864fe8d748
ha1f <half.ldk.hiz@gmail.com>
2019-05-08 00:02:57 +0900

    Initial Commit

---
+++ LeftBarSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeftBarSample
+//
+//  Created by はるふ on 2019/05/08.
+//  Copyright © 2019 はるふ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
